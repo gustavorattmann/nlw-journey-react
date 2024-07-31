30d28ffcdee11300c75e3326e0b0e7a2ff15416d
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2024-07-30 21:10:33 -0300

    CORS fixed

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,15 @@ export default defineConfig(({ mode }) => {
   return {
     server: {
       proxy: {
-        "/api":
-          env?.VITE_API_BASE_URL ||
-          "https://nlw-journey-node-yzzp.onrender.com/" ||
-          "http://localhost:3333",
+        "/api": {
+          target:
+            env?.VITE_API_BASE_URL ||
+            "https://nlw-journey-node-yzzp.onrender.com/" ||
+            "http://localhost:3333",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
     plugins: [react()],
